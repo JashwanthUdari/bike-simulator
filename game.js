2804353624206a8cdc528219a2a42fa114efc57e
Jashwanth Udari <jashwanthudari@hotmail.com>
2026-01-20 17:27:35 +0530

    fixed car size

--- game.js
+++ game.js
@@ -109,7 +109,7 @@ class BikeScene extends Phaser.Scene {
         const key = this.isNight ? "car_night" : "car_day";
 
         const car = this.cars.create(laneX, -80, key);
-        car.setScale(0.40);
+        car.setScale(1.0);
         car.setVelocityY(this.baseCarSpeed);
       }
     });
