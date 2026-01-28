2cd208b2277dd89666056ac36cf5109ad7612d7c
Jashwanth Udari <jashwanthudari@hotmail.com>
2026-01-28 22:40:33 +0530

    Text update

--- game.js
+++ game.js
@@ -194,18 +194,12 @@ this.lanes = [
       ).setOrigin(0.5),
 
       this.add.text(0, 0,
-<<<<<<< HEAD
         "Be Indian driver\nExpect the unexpected traffic 😉",
-=======
-        "Be an Indian driver \n 
-        Expect the unexpected traffic 😉",
->>>>>>> e542ece22259915b0fbefabda004035a4be31188
         {
           fontSize: "18px",
           color: "#f8f3f3",
           align: "center",
-          wordWrap: { width: 320 },
-          lineSpacing: 6
+          wordWrap: { width: 320 }
         }
       ).setOrigin(0.5),
 
